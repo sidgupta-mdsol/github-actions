3b1cbb0d1f8d5066ebe848ef1aecba68f1901c25
Siddharth Gupta <sidgupta@mdsol.com>
2024-06-24 13:07:58 +0530

    Update square.py

--- square.py
+++ square.py
@@ -6,4 +6,4 @@ print(calc_square(5))
 print(os.getenv('Actor'))
 ICON_MDSOL = "/home/runner/work/github-actions/github-actions/docs/icon-mdsol2.png"
 ICON_SUCCESS = "/home/runner/work/github-actions/github-actions/docs/icon-success.png"
-print(ICON_MDSOL ICON_SUCCESS)
+print(ICON_MDSOL, ICON_SUCCESS)
